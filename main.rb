62a0b3020c125e46b7fdc8e5c226ba308cca7962
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-09-17 23:49:51 +0900

    some fixes 💊

--- main.rb
+++ main.rb
@@ -1,19 +1,18 @@
 #!/usr/bin/ruby
 
 require 'net/http'
-
-DEV_ID = open("./.developer_id").read
-ACCESS_TOKEN = open("./.access_token").read
+require 'json'
 
 uri = URI.parse("https://api.iijmio.jp/mobile/d/v2/log/packet/")
 req = Net::HTTP::Get.new(uri)
 
-req["X-IIJmio-Developer"] = DEV_ID
-req["X-IIJmio-Authorization"] = ACCESS_TOKEN
+req["X-IIJmio-Developer"] = open("./.developer_id").read
+req["X-IIJmio-Authorization"] = open("./.access_token").read
 
 https = Net::HTTP.new(uri.host, uri.port)
 https.use_ssl = true
 
 response = https.request(req)
 
-puts response.body
+json = JSON.parse(response.body)
+puts JSON.pretty_generate(json)
